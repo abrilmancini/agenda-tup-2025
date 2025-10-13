9d02541a7c618c7e1fb990d0b89a74a724adb290
abrilmancini <abrilmancini8@gmail.com>
2025-10-13 19:56:28 -0300

    grupo

--- src/app/pages/groups-list-page/groups-list-page.ts
+++ src/app/pages/groups-list-page/groups-list-page.ts
@@ -2,16 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { GrupoService } from '../../services/grupo-service'; 
 import { Grupo } from '../../interfaces/grupo'; 
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-groups-list-page',
   templateUrl: './groups-list-page.html',
   styleUrls: ['./groups-list-page.scss'],
-  imports:[CommonModule]
+  imports:[CommonModule,FormsModule]
 })
 export class GroupsListPage implements OnInit {
   grupos: Grupo[] = []; 
   cargando = true;
+  mostrarFormulario = false;
+  nuevoGrupo: Grupo = { id: 0, nombre: '' };
 
   constructor(private grupoService: GrupoService) {}
 
@@ -28,5 +31,17 @@ export class GroupsListPage implements OnInit {
       }
     });
   }
+  crearGrupo() {
+  if (!this.nuevoGrupo.nombre.trim()) return; // evita guardar vacío
+
+  const nuevo = { ...this.nuevoGrupo, id: Date.now() }; // crea nuevo grupo temporal
+  this.grupos.push(nuevo); // lo agrega al listado
+  this.nuevoGrupo = { id: 0, nombre: '' }; // limpia el formulario
+  this.mostrarFormulario = false; // cierra el form
+}
+cancelar() {
+  this.nuevoGrupo = { id: 0, nombre: '' }; // limpia datos
+  this.mostrarFormulario = false; // oculta formulario
+}
 }
 
