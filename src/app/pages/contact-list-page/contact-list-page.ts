e04dc0ad570f00cff61c78fad22e3c7e1527cc4d
abrilmancini <abrilmancini8@gmail.com>
2025-10-15 12:18:53 -0300

    tabla contactos

--- src/app/pages/contact-list-page/contact-list-page.ts
+++ src/app/pages/contact-list-page/contact-list-page.ts
@@ -4,14 +4,22 @@ import { ContactListItem } from '../../components/contact-list-item/contact-list
 import { Auth } from '../../services/auth';
 import { ContactsService } from '../../services/contacts-service';
 import { FormsModule } from '@angular/forms';
-
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-contact-list-page',
-  imports: [RouterModule, FormsModule],
+  imports: [RouterModule, FormsModule,CommonModule,MatIconModule],
   templateUrl: './contact-list-page.html',
   styleUrl: './contact-list-page.scss'
 })
 export class ContactListPage implements OnInit {
+contact: any;
+deleteContact(arg0: any) {
+throw new Error('Method not implemented.');
+}
+editContact(arg0: any) {
+throw new Error('Method not implemented.');
+}
   ngOnInit(): void {
     this.contactsService.getContacts();
   }
