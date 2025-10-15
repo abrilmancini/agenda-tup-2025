33d04bfa1a6711d725b7737fce43c1359dcdd89a
abrilmancini <abrilmancini8@gmail.com>
2025-10-15 12:34:26 -0300

    tabla contactos

--- src/app/pages/contact-list-page/contact-list-page.ts
+++ src/app/pages/contact-list-page/contact-list-page.ts
@@ -1,6 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { ContactListItem } from '../../components/contact-list-item/contact-list-item';
 import { Auth } from '../../services/auth';
 import { ContactsService } from '../../services/contacts-service';
 import { FormsModule } from '@angular/forms';
